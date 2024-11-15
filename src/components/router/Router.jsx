6f466fe25315434ffd8a7cfa3fefe9bdbe0a8dc9
user <user@Air-de-user.lan>
2024-11-15 11:33:15 +0100

    modifier couleur de heder

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom' //une function pour creer un routor pour lie les pages entre elles
 import Home from '../../pages/home/Home'
 import Apropos from '../../pages/apropos/Apropos'
 import Error from '../../pages/error/Error'
